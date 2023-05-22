d321b6b47af1f5976b75d7095d0a7a4672a394a0
ywt114 <58467878+ywt114@users.noreply.github.com>
2023-05-23 07:29:49 +0800

    Update diy-part2_5.10.sh

--- diy-part2_5.10.sh
+++ diy-part2_5.10.sh
@@ -45,42 +45,33 @@ curl -fsSL https://raw.githubusercontent.com/ywt114/poweroff/main/system.lua > f
 
 # 删除替换默认源插件
 rm -rf feeds/packages/lang/golang
-git clone -b 19.x https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
+svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
 rm -rf feeds/packages/net/smartdns
-git clone https://github.com/pymumu/openwrt-smartdns feeds/packages/net/smartdns
+svn export https://github.com/pymumu/openwrt-smartdns/trunk feeds/packages/net/smartdns
 rm -rf feeds/packages/net/adguardhome
 svn export https://github.com/kenzok8/openwrt-packages/trunk/adguardhome feeds/packages/net/adguardhome
+rm -rf feeds/packages/net/socat
+svn export https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/op-socat/socat feeds/packages/net/socat
 
 # 添加插件
 cd package/lean
-git clone https://github.com/ywt114/luci-app-advanced
-git clone https://github.com/sirpdboy/luci-app-autotimeset
+svn export https://github.com/ywt114/luci-app-advanced/trunk ./luci-app-advanced
+svn export https://github.com/pymumu/luci-app-smartdns/trunk ./luci-app-smartdns
+svn export https://github.com/sirpdboy/chatgpt-web/trunk ./luci-app-chatgpt
+svn export https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus ./luci-app-ssr-plus
+svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall ./luci-app-passwall
+svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash ./luci-app-openclash
+svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome ./luci-app-adguardhome
+svn export https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/op-socat/luci-app-socat ./luci-app-socat
+svn export https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/op-homebox ./op-homebox
+svn export https://github.com/sbwml/luci-app-alist/trunk ./op-alist
+svn export https://github.com/sirpdboy/luci-app-autotimeset/trunk ./luci-app-autotimeset
 sed -i 's/control"/system"/g' luci-app-autotimeset/luasrc/controller/autotimeset.lua
-git clone https://github.com/sbwml/luci-app-alist
-git clone -b lede https://github.com/pymumu/luci-app-smartdns
-git clone https://github.com/kenzok8/small
+svn export https://github.com/linkease/istore/trunk ./istore
+sed -i 's/+luci-lib-ipkg/+luci-base/g' istore/luci/luci-app-store/Makefile
+svn export https://github.com/kenzok8/small/trunk ./small
 rm -rf small/luci-app-bypass
 rm -rf small/luci-app-passwall
 rm -rf small/luci-app-passwall2
 rm -rf small/luci-app-ssr-plus
 rm -rf small/luci-app-vssr
-git clone https://github.com/fw876/helloworld
-\cp -rf helloworld/luci-app-ssr-plus .
-rm -rf helloworld
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall
-\cp -rf openwrt-passwall/luci-app-passwall .
-rm -rf openwrt-passwall
-git clone https://github.com/vernesong/OpenClash
-\cp -rf OpenClash/luci-app-openclash .
-rm -rf OpenClash
-git clone https://github.com/xiangfeidexiaohuo/openwrt-packages
-\cp -rf openwrt-packages/op-homebox .
-rm -rf openwrt-packages/op-socat/socat
-\cp -rf openwrt-packages/op-socat .
-rm -rf openwrt-packages
-git clone https://github.com/kenzok8/openwrt-packages
-\cp -rf openwrt-packages/luci-app-adguardhome .
-rm -rf openwrt-packages
-git clone https://github.com/linkease/istore
-sed -i 's/+luci-lib-ipkg/+luci-base/g' istore/luci/luci-app-store/Makefile
-git clone https://github.com/sirpdboy/chatgpt-web luci-app-chatgpt/
