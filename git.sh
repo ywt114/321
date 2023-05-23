985bd5a89f4c6cbe9d92384d1eaa1df44c39370c
ywt114 <58467878+ywt114@users.noreply.github.com>
2023-05-23 10:06:12 +0800

    Update git.sh

--- git.sh
+++ git.sh
@@ -46,35 +46,74 @@ curl -fsSL https://raw.githubusercontent.com/ywt114/poweroff/main/system.lua > f
 # 删除替换默认源插件
 rm -rf feeds/packages/lang/golang
 svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
+git clone -b 19.x https://github.com/sbwml/packages_lang_golang.git feeds/packages/lang/golang
+
 rm -rf feeds/packages/net/smartdns
 svn export https://github.com/pymumu/openwrt-smartdns/trunk feeds/packages/net/smartdns
+git clone https://github.com/pymumu/openwrt-smartdns.git feeds/packages/net/smartdns
+
 rm -rf feeds/packages/net/adguardhome
 svn export https://github.com/kenzok8/openwrt-packages/trunk/adguardhome feeds/packages/net/adguardhome
+git clone https://github.com/kenzok8/openwrt-packages.git feeds/packages/net/adguardhome
+
 rm -rf feeds/packages/net/socat
 svn export https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/op-socat/socat feeds/packages/net/socat
+git clone https://github.com/xiangfeidexiaohuo/openwrt-packages.git feeds/packages/net/socat
+
 rm -rf feeds/packages/net/mosdns
 svn export https://github.com/kenzok8/openwrt-packages/trunk/mosdns feeds/packages/net/mosdns
+git clone https://github.com/kenzok8/openwrt-packages.git feeds/packages/net/mosdns
+
 rm -rf feeds/packages/utils/v2dat
 svn export https://github.com/kenzok8/openwrt-packages/trunk/v2dat feeds/packages/utils/v2dat
+git clone https://github.com/kenzok8/openwrt-packages.git feeds/packages/utils/v2dat
 
 # 添加插件
 cd package/lean
 svn export https://github.com/ywt114/luci-app-advanced/trunk ./luci-app-advanced
+git clone https://github.com/ywt114/luci-app-advanced.git ./luci-app-advanced
+
 svn export https://github.com/pymumu/luci-app-smartdns/trunk ./luci-app-smartdns
+git clone https://github.com/pymumu/luci-app-smartdns.git ./luci-app-smartdns
+
 svn export https://github.com/sirpdboy/chatgpt-web/trunk ./luci-app-chatgpt
+git clone https://github.com/sirpdboy/chatgpt-web.git ./luci-app-chatgpt
+
 svn export https://github.com/fw876/helloworld/trunk/luci-app-ssr-plus ./luci-app-ssr-plus
+git clone https://github.com/fw876/helloworld.git ./luci-app-ssr-plus
+
 svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall ./luci-app-passwall
+git clone --branch luci https://github.com/xiaorouji/openwrt-passwall.git ./luci-app-passwall
+
 svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash ./luci-app-openclash
+git clone https://github.com/vernesong/OpenClash.git ./luci-app-openclash
+
 svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome ./luci-app-adguardhome
+git clone https://github.com/kenzok8/openwrt-packages.git ./luci-app-adguardhome
+
 svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-mosdns ./luci-app-mosdns
+git clone https://github.com/kenzok8/openwrt-packages.git ./luci-app-mosdns
+
 svn export https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/op-socat/luci-app-socat ./luci-app-socat
+git clone https://github.com/xiangfeidexiaohuo/openwrt-packages.git ./luci-app-socat
+
 svn export https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/op-homebox ./op-homebox
+git clone https://github.com/xiangfeidexiaohuo/openwrt-packages.git ./op-homebox
+
 svn export https://github.com/sbwml/luci-app-alist/trunk ./op-alist
+git clone https://github.com/sbwml/luci-app-alist.git ./op-alist
+
 svn export https://github.com/sirpdboy/luci-app-autotimeset/trunk ./luci-app-autotimeset
+git clone https://github.com/sirpdboy/luci-app-autotimeset.git ./luci-app-autotimeset
+
 sed -i 's/control"/system"/g' luci-app-autotimeset/luasrc/controller/autotimeset.lua
 svn export https://github.com/linkease/istore/trunk ./istore
+git clone https://github.com/linkease/istore.git ./istore
+
 sed -i 's/+luci-lib-ipkg/+luci-base/g' istore/luci/luci-app-store/Makefile
 svn export https://github.com/kenzok8/small/trunk ./small
+git clone https://github.com/kenzok8/small.git ./small
+
 rm -rf small/luci-app-bypass
 rm -rf small/luci-app-passwall
 rm -rf small/luci-app-passwall2
